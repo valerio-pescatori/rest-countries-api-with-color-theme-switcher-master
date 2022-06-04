a0f7156ebf136827904f19daaba63bbe2b3e113d
valerio-pescatori <pescatorim14@gmail.com>
2022-06-04 17:14:58 +0200

    removed useless folder nesting

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: "./",
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
