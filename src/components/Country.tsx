e57dba47aa1bd1e720a2a56310efbe3361246e6b
valerio-pescatori <pescatorim14@gmail.com>
2022-06-14 12:56:27 +0200

    Done Country component ultimated

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,8 +1,7 @@
-import { Component, createResource, For, Show } from "solid-js";
+import { Component, createEffect, createResource, For, onMount, Show } from "solid-js";
 import styles from "./Country.module.css";
-import containerStyles from "../App.module.css";
 import { RestCountry } from "../CountriesInterface";
-import { useParams } from "solid-app-router";
+import { Link, useParams } from "solid-app-router";
 import { BASE_API_URL, formatNumber } from "../index";
 
 async function fetchCountry(id: string): Promise<RestCountry> {
@@ -11,59 +10,88 @@ async function fetchCountry(id: string): Promise<RestCountry> {
   return arr[0];
 }
 
+async function getCountryNameByAlpha(alphaCodes: string[]): Promise<String[]> {
+  let promises: Promise<RestCountry>[] = alphaCodes.map(async (alpha) => {
+    let response: Response = await fetch(BASE_API_URL + "alpha/" + alpha);
+    let country = await response.json();
+    return country[0];
+  });
+  let responses: RestCountry[] = await Promise.all(promises);
+  return responses.map((el) => el.name.common);
+}
+
 const Country: Component = (props) => {
   const params = useParams();
   const [country] = createResource(() => params.id, fetchCountry);
+  const [borders] = createResource(() => country()?.borders?.slice(0, 3), getCountryNameByAlpha);
 
   return (
     <Show when={country()} fallback={<h1 style="text-align: center; margin-top: 2em">Loading...</h1>}>
-      <header class={containerStyles.container}>
-        <div class={styles.button}>
-          <i class="fal fa-arrow-left"></i>
-          Back
-        </div>
-      </header>
-      <div class={containerStyles.container} style="justify-content: start">
-        <img class={styles.cardImg} src={country()!.flags.png}></img>
-        <div class={styles.cardBody}>
-          <h1 class={styles.cardTitle}>{country()!.name.common}</h1>
-          <div class={styles.cardParagraphContainer}>
-            <p class={styles.cardParagraph}>
-              <span style="font-weight: bold">Native Name: </span>
-              {Object.values(country()!.name.nativeName)[0].common}
-            </p>
-            <p class={styles.cardParagraph}>
-              <span style="font-weight: bold">Population: </span>
-              {formatNumber(country()!.population)}
-            </p>
-            <p class={styles.cardParagraph}>
-              <span style="font-weight: bold">Region: </span>
-              {country()!.region}
-            </p>
-            <p class={styles.cardParagraph}>
-              <span style="font-weight: bold">Sub Region: </span>
-              {country()!.subregion}
-            </p>
-            <p class={styles.cardParagraph}>
-              <span style="font-weight: bold">Capital: </span>
-              {country()!.capital}
-            </p>
+      <header class={styles.container}>
+        <Link href="/">
+          <div class={styles.button}>
+            <i class="fal fa-arrow-left"></i>
+            Back
           </div>
+        </Link>
+      </header>
+      <div class={styles.container}>
+        {/* Image */}
+        <img class={styles.cardImg} src={country()!.flags.png}></img>
+        {/* Body */}
+        <div class={styles.cardBody}>
+          {/* Title */}
+          <h1 class={styles.cardTitle}>{country()!.name.common}</h1>
+          {/* Content */}
           <div class={styles.cardParagraphContainer}>
-            <p class={styles.cardParagraph}>
-              <span style="font-weight: bold">Top Level Domain: </span>
-              {country()!.tld[0]}
-            </p>
-            <p class={styles.cardParagraph}>
-              <span style="font-weight: bold">Currency: </span>
-              {Object.values(country()!.currencies)[0].name}
-            </p>
-            <p class={styles.cardParagraph}>
-              <span style="font-weight: bold">Languages: </span>
-              <For each={Object.values(country()!.languages)}>
-                {(el, i) => (i() == Object.values(country()!.languages).length - 1 ? el : el + ", ")}
+            <div class={styles.cardParagraph}>
+              <p class={styles.cardParagraph}>
+                <span style="font-weight: bold">Native Name: </span>
+                {Object.values(country()!.name.nativeName)[0].common}
+              </p>
+              <p class={styles.cardParagraph}>
+                <span style="font-weight: bold">Population: </span>
+                {formatNumber(country()!.population)}
+              </p>
+              <p class={styles.cardParagraph}>
+                <span style="font-weight: bold">Region: </span>
+                {country()!.region}
+              </p>
+              <p class={styles.cardParagraph}>
+                <span style="font-weight: bold">Sub Region: </span>
+                {country()!.subregion}
+              </p>
+              <p class={styles.cardParagraph}>
+                <span style="font-weight: bold">Capital: </span>
+                {country()!.capital}
+              </p>
+            </div>
+            <div class={styles.cardParagraph}>
+              <p>
+                <span style="font-weight: bold">Top Level Domain: </span>
+                {country()!.tld[0]}
+              </p>
+              <p>
+                <span style="font-weight: bold">Currency: </span>
+                {Object.values(country()!.currencies)[0].name}
+              </p>
+              <p>
+                <span style="font-weight: bold">Languages: </span>
+
+                <For each={Object.values(country()!.languages)} fallback={<span>Loading...</span>}>
+                  {(el, i) => (i() == Object.values(country()!.languages).length - 1 ? el : el + ", ")}
+                </For>
+              </p>
+            </div>
+          </div>
+          {/* Borders*/}
+          <div class={styles.cardBorders}>
+            <p style="font-weight: bold">Border Countries: </p>
+            <div>
+              <For each={borders()} fallback={<span> None</span>}>
+                {(border) => <span class={styles.cardBorder}>{border + " "}</span>}
               </For>
-            </p>
+            </div>
           </div>
         </div>
       </div>
